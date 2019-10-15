64b579f7be1d07c70b37bf97bacd93adf3bcd7fe
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2019-10-15 07:07:37 +0100

    sum changes1
    cambios que no me importaría perder xD pero que quiero guardar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,18 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script> 
+    <script src="semantic/dist/semantic.min.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 </head>
 <body>
     <div id="app">
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,15 @@ Route::get('/', function () {
     return view('Users/welcome');
 });
 
+Route::get('/trabajos', function () {
+    return view('Users/welcome');
+});
+
+Route::get('/trabajo', function () {
+    return view('Users/welcome');
+});
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
